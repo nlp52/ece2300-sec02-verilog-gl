bc2b3368495d6ec6e02b13dc83d7fbc06de745f9
nlp52 <nlp52@cornell.edu>
2024-09-18 20:44:39 -0400

    finished implementation

--- PairTripleDetector_GL.v
+++ PairTripleDetector_GL.v
@@ -11,11 +11,21 @@ module PairTripleDetector_GL
   input  wire in1,
   input  wire in2,
   output wire out
+
 );
 
   //''' ACTIVITY '''''''''''''''''''''''''''''''''''''''''''''''''''''''''
   // Implement pair/triple detector using explicit gate-level modeling
   //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+  wire w;
+  wire y;
+  wire x; 
+  or top_or (w, in0, in1);
+  and middle_and (y, w, in2);
+  and bottom_and (x, in0, in1);
+  or last_or (out, y, x);
+
 
 endmodule
 
